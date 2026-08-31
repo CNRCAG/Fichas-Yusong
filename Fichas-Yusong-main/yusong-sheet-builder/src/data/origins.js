763797af082764a1330c0dcb18e893e41581bee8
CnrcGM <cristhianoag@gmail.com>
2026-08-31 13:54:21 -0300

    V0.1 D&D
    continuar na escola

--- Fichas-Yusong-main/yusong-sheet-builder/src/data/origins.js
+++
@@ -1,4 +0,0 @@
-export const origins = [
-  { name: "Lutador Profissional", benefit: "Perícias: +1 em Fama e Fortitude. Habilidade: Campeão Você é profissional ou apenas chegou na última etapa do seu arte marcial (Ex: Faixa Preta) e você sabe até ensinar ele caso precise. Você tem vantagens em testes para ensinar sua arte marcial e pode gastar 25% da Stamina Máxima para subir o nível dos seus dados em um nível enquanto estiver em postura de luta." }
-  // Adicione as outras origens do suplemento conforme necessário
-];
